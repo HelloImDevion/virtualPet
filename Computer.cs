8f11c81a5a7ad6b5b307631f136e313cfb6fed89
HelloImDevion <devionfothergill@gmail.com>
2017-03-02 15:55:26 -0500

    first commit

---
+++ Computer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualPet
+{
+    class Pet
+    {
+        private int eat;
+
+        private int fly;
+
+        private int sleep;
+
+        private int train;
+
+        private int play;
+
+        public Pet()
+
+        {
+           
+
+        }
+
+        public Pet(int Eat, int fly, int sleep, int train, int play)
+        {
+            this.eat = eat;
+            this.fly = fly;
+            this.sleep = sleep;
+            this.train = train;
+            this.play = play;
+
+
+        }
+
+        public int Eat
+        {
+            get { return this.eat; }
+            set { eat = value; }
+        }
+
+        public int Fly
+        {
+            get { return this.fly; }
+            set { fly = value; }
+        }
+
+        public int Sleep
+        {
+            get { return this.sleep; }
+            set { sleep = value; }
+        }
+
+        public int Train
+        {
+            get { return this.train; }
+            set { train = value; }
+        }
+
+        public int Play
+        {
+            get { return this.play; }
+            set { play = value; }
+            
+        }
+
+        static int Play()
+        {
+            Console.WriteLine("Your dragon is playing");
+        } 
+
+        static int Train()
+        {
+            Console.WriteLine("Your dragon is stronger!");
+        }
+
+        static int Sleep()
+        {
+            Console.WriteLine("Shhhhhh! your dragon is sleep. ");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("eat = 1");
+            list.Add("fly = 2");
+            list.Add("sleep = 3");
+            list.Add("train = 4");
+            list.Add("play = 5");
+
+            foreach (string currentVal in list)
+            {
+                Console.WriteLine(currentVal);
+            }
+
+            {
+                Console.WriteLine("Insert your needs: ");
+                string userInPut = Console.ReadLine();
+            }
+        }
+    }
+}
